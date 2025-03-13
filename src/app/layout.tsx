0651ee670c0c377d8a565c9ec3cb4b5bf79c942b
Tanvirgeek <tanvirgeek@gmail.com>
2025-03-13 14:31:10 +0600

    working on navigationbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import NavigationBar from "@/components/NavigationBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +28,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <div>
+          <NavigationBar />
+          {children}
+        </div>
       </body>
     </html>
   );
---
+++ src/components/NavigationBar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavigationBar = () => {
+  return <div>Hi, I am navigation bar</div>;
+};
+
+export default NavigationBar;
