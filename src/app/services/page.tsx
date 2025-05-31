6a3ceb85c1932fe5ab788af209138b774241912f
Tanvirgeek <tanvirgeek@gmail.com>
2025-05-31 11:31:42 +0600

    services

--- src/app/services/page.tsx
+++ src/app/services/page.tsx
@@ -2,6 +2,7 @@ import ExcitedToStart from "@/components/ExcitedToStart";
 import PageHero from "@/components/PageHero";
 import ContactHero from "@/components/PageHero";
 import PageHeroImage from "@/components/PageHeroImage";
+import Services from "@/components/Services";
 import React from "react";
 
 const Service = () => {
@@ -9,6 +10,7 @@ const Service = () => {
     <div>
       <PageHero heading={`Building Timeless\nSpaces`} text="our services" />
       <PageHeroImage imageUrl={"/contact-hero.png"} />
+      <Services />
       <ExcitedToStart />
     </div>
   );
