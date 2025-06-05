955ddc9dd6a1d4ad7c9452fd0f58444111139e71
Tanvirgeek <tanvirgeek@gmail.com>
2025-06-05 11:42:30 +0600

    Padding fixed in large screen

--- src/components/Home2HeroBottom.tsx
+++ src/components/Home2HeroBottom.tsx
@@ -3,7 +3,7 @@ import Home2HeroCard from "./Home2HeroCard";
 
 const Home2HeroBottom = () => {
   return (
-    <div className="bg-foreground md:pt-80">
+    <div className="bg-foreground md:pt-96">
       <div className="app-section container">
         <div className="flex flex-col md:flex-row">
           <Home2HeroCard
